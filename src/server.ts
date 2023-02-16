0a22e666773af875781319e652d0e53e9cf83d8e
adam-almeida <adamportifolio@gmail.com>
2023-02-15 22:40:13 -0300

    feat: create transactions migration

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,11 @@ import { knex } from './database'
 const app = fastify()
 
 app.get('/hello', async () => {
-  const test = knex('sqlite_schema').select('*')
-  return test
+  const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transaction
 })
 
 app
